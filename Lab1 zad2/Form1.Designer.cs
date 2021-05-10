7ec002b805f0f7897fbd138f946ecfd6c567f926
Tomasz Sagan <w62950@student.wsiz.edu.pl>
2021-05-10 20:36:33 +0200

    Add project files.

---
+++ Lab1 zad2/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace Lab1_zad2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.brownanie = new System.Windows.Forms.Button();
+            this.bdodawanie = new System.Windows.Forms.Button();
+            this.bodejmowanie = new System.Windows.Forms.Button();
+            this.bmnozenie = new System.Windows.Forms.Button();
+            this.bdzielenie = new System.Windows.Forms.Button();
+            this.bczyszczenie = new System.Windows.Forms.Button();
+            this.bprzecinek = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(397, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(28, 77);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(80, 55);
+            this.b1.TabIndex = 1;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(136, 77);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(78, 55);
+            this.b2.TabIndex = 2;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(243, 77);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(77, 55);
+            this.b3.TabIndex = 3;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(28, 148);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(80, 54);
+            this.b4.TabIndex = 4;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(136, 148);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(78, 54);
+            this.b5.TabIndex = 5;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(243, 148);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(77, 54);
+            this.b6.TabIndex = 6;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(28, 224);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(80, 54);
+            this.b7.TabIndex = 7;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(136, 224);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(78, 54);
+            this.b8.TabIndex = 8;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(243, 224);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(77, 54);
+            this.b9.TabIndex = 9;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(30, 300);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(78, 54);
+            this.b0.TabIndex = 10;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.b0_Click);
+            // 
+            // brownanie
+            // 
+            this.brownanie.Location = new System.Drawing.Point(348, 224);
+            this.brownanie.Name = "brownanie";
+            this.brownanie.Size = new System.Drawing.Size(77, 54);
+            this.brownanie.TabIndex = 11;
+            this.brownanie.Text = "=";
+            this.brownanie.UseVisualStyleBackColor = true;
+            this.brownanie.Click += new System.EventHandler(this.brownanie_Click);
+            // 
+            // bdodawanie
+            // 
+            this.bdodawanie.Location = new System.Drawing.Point(243, 300);
+            this.bdodawanie.Name = "bdodawanie";
+            this.bdodawanie.Size = new System.Drawing.Size(77, 55);
+            this.bdodawanie.TabIndex = 12;
+            this.bdodawanie.Text = "+";
+            this.bdodawanie.UseVisualStyleBackColor = true;
+            this.bdodawanie.Click += new System.EventHandler(this.bdodawanie_Click);
+            // 
+            // bodejmowanie
+            // 
+            this.bodejmowanie.Location = new System.Drawing.Point(348, 300);
+            this.bodejmowanie.Name = "bodejmowanie";
+            this.bodejmowanie.Size = new System.Drawing.Size(77, 54);
+            this.bodejmowanie.TabIndex = 13;
+            this.bodejmowanie.Text = "-";
+            this.bodejmowanie.UseVisualStyleBackColor = true;
+            this.bodejmowanie.Click += new System.EventHandler(this.bodejmowanie_Click);
+            // 
+            // bmnozenie
+            // 
+            this.bmnozenie.Location = new System.Drawing.Point(348, 383);
+            this.bmnozenie.Name = "bmnozenie";
+            this.bmnozenie.Size = new System.Drawing.Size(77, 54);
+            this.bmnozenie.TabIndex = 14;
+            this.bmnozenie.Text = "*";
+            this.bmnozenie.UseVisualStyleBackColor = true;
+            this.bmnozenie.Click += new System.EventHandler(this.bmnozenie_Click);
+            // 
+            // bdzielenie
+            // 
+            this.bdzielenie.Location = new System.Drawing.Point(243, 383);
+            this.bdzielenie.Name = "bdzielenie";
+            this.bdzielenie.Size = new System.Drawing.Size(77, 54);
+            this.bdzielenie.TabIndex = 15;
+            this.bdzielenie.Text = "/";
+            this.bdzielenie.UseVisualStyleBackColor = true;
+            this.bdzielenie.Click += new System.EventHandler(this.bdzielenie_Click);
+            // 
+            // bczyszczenie
+            // 
+            this.bczyszczenie.Location = new System.Drawing.Point(348, 77);
+            this.bczyszczenie.Name = "bczyszczenie";
+            this.bczyszczenie.Size = new System.Drawing.Size(77, 125);
+            this.bczyszczenie.TabIndex = 16;
+            this.bczyszczenie.Text = "C";
+            this.bczyszczenie.UseVisualStyleBackColor = true;
+            this.bczyszczenie.Click += new System.EventHandler(this.bczyszczenie_Click);
+            // 
+            // bprzecinek
+            // 
+            this.bprzecinek.Location = new System.Drawing.Point(136, 300);
+            this.bprzecinek.Name = "bprzecinek";
+            this.bprzecinek.Size = new System.Drawing.Size(78, 54);
+            this.bprzecinek.TabIndex = 17;
+            this.bprzecinek.Text = ",";
+            this.bprzecinek.UseVisualStyleBackColor = true;
+            this.bprzecinek.Click += new System.EventHandler(this.bprzecinek_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 468);
+            this.Controls.Add(this.bprzecinek);
+            this.Controls.Add(this.bczyszczenie);
+            this.Controls.Add(this.bdzielenie);
+            this.Controls.Add(this.bmnozenie);
+            this.Controls.Add(this.bodejmowanie);
+            this.Controls.Add(this.bdodawanie);
+            this.Controls.Add(this.brownanie);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button brownanie;
+        private System.Windows.Forms.Button bdodawanie;
+        private System.Windows.Forms.Button bodejmowanie;
+        private System.Windows.Forms.Button bmnozenie;
+        private System.Windows.Forms.Button bdzielenie;
+        private System.Windows.Forms.Button bczyszczenie;
+        private System.Windows.Forms.Button bprzecinek;
+    }
+}
+
---
+++ Lab1 zad2/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1_zad2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string LiczbaPierwsza, LiczbaDruga;
+        char dzialanie = ' ';
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            Dzialanie(1);
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            Dzialanie(2);
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            Dzialanie(3);
+        }
+
+        private void b4_Click(object sender, EventArgs e)
+        {
+            Dzialanie(4);
+        }
+
+        private void b5_Click(object sender, EventArgs e)
+        {
+            Dzialanie(5);
+        }
+
+        private void b6_Click(object sender, EventArgs e)
+        {
+            Dzialanie(6);
+        }
+
+        private void b7_Click(object sender, EventArgs e)
+        {
+            Dzialanie(7);
+        }
+
+        private void b8_Click(object sender, EventArgs e)
+        {
+            Dzialanie(8);
+        }
+
+        private void b9_Click(object sender, EventArgs e)
+        {
+            Dzialanie(9);
+        }
+
+        private void b0_Click(object sender, EventArgs e)
+        {
+            Dzialanie(0);
+        }
+
+        private void brownanie_Click(object sender, EventArgs e)
+        {
+            switch (dzialanie)
+            {
+                case ('+'):
+                    textBox1.Text = (int.Parse(LiczbaPierwsza) + int.Parse(LiczbaDruga)).ToString();
+                    break;
+                case ('-'):
+                    textBox1.Text = (int.Parse(LiczbaPierwsza) - int.Parse(LiczbaDruga)).ToString();
+                    break;
+                case ('*'):
+                    textBox1.Text = (int.Parse(LiczbaPierwsza) * int.Parse(LiczbaDruga)).ToString();
+                    break;
+                case ('/'):
+                    textBox1.Text = (int.Parse(LiczbaPierwsza) / int.Parse(LiczbaDruga)).ToString();
+                    break;
+            }
+            LiczbaPierwsza = "";
+            LiczbaDruga = "";
+            dzialanie = ' ';
+        }
+
+        private void bdodawanie_Click(object sender, EventArgs e)
+        {
+            dzialanie = '+';
+            textBox1.Text = "";
+        }
+
+        private void bodejmowanie_Click(object sender, EventArgs e)
+        {
+            dzialanie = '-';
+            textBox1.Text = "";
+        }
+
+        private void bmnozenie_Click(object sender, EventArgs e)
+        {
+            dzialanie = '*';
+            textBox1.Text = "";
+        }
+
+        private void bczyszczenie_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void bprzecinek_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + ",";
+            textBox1.Text = Convert.ToString(textBox1.Text);
+        }
+
+        private void bdzielenie_Click(object sender, EventArgs e)
+        {
+            dzialanie = '/';
+            textBox1.Text = "";
+        }
+
+        private void Dzialanie(int liczba)
+        {
+            if (dzialanie == ' ')
+            {
+                LiczbaPierwsza += liczba;
+                textBox1.Text = LiczbaPierwsza;
+            }
+            else
+            {
+                LiczbaDruga += liczba;
+                textBox1.Text = LiczbaDruga;
+            }
+        }
+    }
+}
